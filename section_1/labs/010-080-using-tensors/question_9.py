0d51d61a2a7d8b86f58d03bef960dcebe8bbd0ce
MOrgayle, 2xNVIDIA, 2xLPIC3, OpenEDG C++ <79911993+morgayle-ngt@users.noreply.github.com>
2025-03-23 09:18:50 +0000

    completed section_1

--- section_1/labs/010-080-using-tensors/question_9.py
+++ section_1/labs/010-080-using-tensors/question_9.py
@@ -4,12 +4,12 @@ If a GPU device exists on the machine, set the device to use it. If a GPU is not
 import torch
 
 # Check if GPU is available to use
-if torch.____.____:
-    device = '____'
+if torch.cuda.is_available():
+  device = 'cuda'
 else:
-    device = '____'
+  device = 'cpu'
 
-# Create a tensor and the device congifured above
+# Create a tensor and the device configured above
 tensor = torch.tensor([4, 90, 90], device=device)
 
 # Print the device of the tensor
